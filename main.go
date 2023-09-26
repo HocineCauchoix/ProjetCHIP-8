d8f51370947ee9eef0ac09a83468d963b6e64a2f
Blackjack773 <jacqueslao03@gmail.com>
2023-09-26 11:54:17 +0200

    Chip8 Const + Struct

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
-import (
-	"fmt"
+const (
+	//4 KB de RAM
+	memorySize = 4096
+	//Nb de Commandes = 16
+	commandCount = 16
+	//Taille de Stack
+	stackSize = 16
+	//Dimension de l'écran
+	screenSize = 64 * 32
 )
 
-func main() {
-	for zz := 97; zz <= 122; zz++ {
-		fmt.Println(rune(zz))
-	}
-	fmt.Println('\n')
+type Chip8 struct {
+	memory  [memorySize]byte   //mémoire du Chip8
+	command [commandCount]byte // cmd V0 à VF
+	//uint16 = non negative ints of 16 bits
+	indexRegister  uint16            // Opérations pour la mémoire
+	programCounter uint16            // Pointeur du programme en execution
+	stack          [stackSize]uint16 // prise en compte du flow de l'execution du programme
+	stackPointer   byte              //Prise en compte du niveau de stack
+	delayTimer     byte              // temps de délai entre chaque évènement
+	screen         [screenSize]byte  // Taille de l'écran du Chip8
 }
