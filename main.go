c61975149417ee25289d9fd11054f761e991007a
Blackjack773 <jacqueslao03@gmail.com>
2023-10-04 16:30:47 +0200

    ajout opcodes

--- main.go
+++ main.go
@@ -2,207 +2,113 @@ package main
 
 import (
 	"fmt"
-	"os"
-
 	"log"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-// variables constantes
 const (
-	//4 KB de RAM
-	memorySize = 4096
-	//Nb de Commandes = 16
+	memorySize   = 4096
 	commandCount = 16
-	//Taille de Stack
-	stackSize = 16
-	//Dimension de l'écran
-	screenSize = 64 * 32
-	OpcodeFX29 = 0xF029
-	OpcodeFX33 = 0xF033
-	OpcodeDXYN = 0xD000
+	stackSize    = 16
+	screenSize   = 64 * 32
+	OpcodeFX29   = 0xF029
+	OpcodeFX33   = 0xF033
+	OpcodeDXYN   = 0xD000
 )
 
-// structure du Chip8
 type Chip8 struct {
-	input       [16]bool // État des touches du clavier (0-F)     // Temporisation du délai
-	soundTimer  byte     // Temporisation du son
-	keyPressed  bool     // Un drapeau pour indiquer si une touche est pressée
-	keyRegister uint16
-	memory      [memorySize]byte   //mémoire du Chip8
-	command     [commandCount]byte // cmd V0 à VF
-	//uint16 = ints entiers non negatifs de 16 bits
-	indexRegister  uint16            // Opérations pour la mémoire
-	programCounter uint16            // Pointeur du programme en execution
-	stack          [stackSize]uint16 // prise en compte du flow de l'execution du programme
-	stackPointer   byte              //Prise en compte du niveau de stack
-	delayTimer     byte              // temps de délai entre chaque évènement // Taille de l'écran du Chip8
-	fontset        [80]byte          // Tableau de police (pour stocker les chiffres 0-9 et certaines lettres)
-	screen         [64 * 32]bool     // Écran représenté par un tableau de booléens
-	pixelUpdate    bool              // Un drapeau pour indiquer si l'écran a été modifié
-
+	Register       [16]uint8
+	input          [16]bool
+	soundTimer     byte
+	keyPressed     bool
+	keyRegister    uint16
+	memory         [memorySize]byte
+	command        [commandCount]byte
+	indexRegister  uint16
+	programCounter uint16
+	stack          [stackSize]uint16
+	stackPointer   byte
+	delayTimer     byte
+	fontset        [80]byte
+	screen         [screenSize]byte
+	pixelUpdate    byte
 }
 
-// fonction de chargement d'un programme Chip8 (ROM)
-func (c *Chip8) loadProgram() error {
-	filename := "game/Tetris.ch8" // Chemin de la ROM
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		return err
+func (c *Chip8) loadProgram(rom []byte) error {
+	if len(rom) > len(c.memory)-0x200 {
+		return fmt.Errorf("ROM is too large to fit in memory")
 	}
 
-	for i, b := range data {
-		c.memory[i+0x200] = b
-	}
-	c.fontset = [80]byte{
-		// Les valeurs de police pour 0-9
-		0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-		0x20, 0x60, 0x20, 0x20, 0x70, // 1
-		0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-		0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-		0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-		0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-		0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-		0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-		0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-		0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-
-		// Les valeurs de police pour A-F (10-15)
-		0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-		0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-		0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-		0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-		0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-		0xF0, 0x80, 0xF0, 0x80, 0x80, // F
-	}
+	copy(c.memory[0x200:], rom)
+	c.programCounter = 0x200
 
 	return nil
 }
-func (c *Chip8) drawSprite(x, y, n uint16) {
-	// Parcourez chaque ligne du sprite (n lignes)
-	for row := uint16(0); row < n; row++ {
-		spriteRow := c.memory[c.indexRegister+row]
 
-		// Parcourez chaque pixel de la ligne (8 pixels par byte)
-		for col := uint16(0); col < 8; col++ {
-			// Vérifiez chaque bit de la ligne du sprite
-			if (spriteRow & (0x80 >> col)) != 0 {
-				// Calculez les coordonnées réelles (modulo pour gérer le débordement)
-				realX := (x + col) % 64
-				realY := (y + row) % 32
-
-				// Mettez à jour l'état du pixel
-				c.screen[realY*64+realX] = !c.screen[realY*64+realX]
-			}
-		}
-	}
-}
-
-func (c *Chip8) emulateCycle() {
-	// Recherche de l'opcode dans la mémoire
+func (c *Chip8) fetchOpcode() {
 	opcode := uint16(c.memory[c.programCounter])<<8 | uint16(c.memory[c.programCounter+1])
-
-	// Décodage et execution de l'opcode (Si plus de codes, ajouter des cas)
 	switch opcode & 0xF000 {
+
 	case 0x0000:
 		switch opcode & 0x00FF {
-		case 0x00E0:
-			// Opcode 0x00E0 : Efface l'écran
-			// Implémentez le code pour effacer l'écran ici
-			// c.screen doit être effacé
-			c.programCounter += 2
 		case 0x00EE:
-			// Opcode 0x00EE : Retour d'une sous-routine
-			// Implémentez le code pour revenir d'une sous-routine ici
-			// Vous devriez dépiler l'adresse de retour depuis la pile
-			// c.programCounter doit être mis à cette adresse
-
-			// Opérateur FX29 : Met le pointeur de police I à l'emplacement du caractère stocké dans VX.
-		case OpcodeFX29:
-			x := uint16((opcode & 0x0F00) >> 8)
-			character := c.command[x]
-			c.indexRegister = uint16(character) * 5 // Chaque caractère a une largeur de 5 pixels
-			c.programCounter += 2
-
-		// Opérateur FX33 : Stocke la représentation binaire-décimale de VX à l'adresse I, I+1 et I+2.
-		case OpcodeFX33:
-			x := uint16((opcode & 0x0F00) >> 8)
-			value := c.command[x]
-			c.memory[c.indexRegister] = value / 100         // Centaines
-			c.memory[c.indexRegister+1] = (value / 10) % 10 // Dizaines
-			c.memory[c.indexRegister+2] = value % 10        // Unités
-			c.programCounter += 2
-
-		// Opérateur DXYN : Dessine un sprite en utilisant les données stockées à l'adresse I à partir de (VX, VY), avec une hauteur de N pixels.
-		case OpcodeDXYN:
-			x := uint16((opcode & 0x0F00) >> 8)
-			y := uint16((opcode & 0x00F0) >> 4)
-			height := uint16(opcode & 0x000F)
-
-			// Vous devrez implémenter la logique pour dessiner le sprite ici,
-			// en utilisant les données à l'adresse I et en mettant à jour l'écran (c.screen).
-			// N'oubliez pas de gérer les collisions et de définir le drapeau pixelUpdate en conséquence.
-
-			c.pixelUpdate = false
-
-			// Dessiner le sprite ligne par ligne
-			for row := uint16(0); row < height; row++ {
-				spriteData := c.memory[c.indexRegister+row]
-
-				// Dessiner chaque pixel du sprite
-				for col := uint16(0); col < 8; col++ {
-					// Utilisez les opérations de bit pour extraire chaque pixel du sprite
-					pixel := (spriteData >> (7 - col)) & 0x1
-					posX := (x + col) % 64
-					posY := (y + row) % 32
-
-					// Si le pixel est allumé (1) et le pixel sur l'écran est déjà allumé (1),
-					// définissez le drapeau de collision (c.pixelUpdate)
-					if pixel == 1 && c.screen[posY*64+posX] {
-						c.pixelUpdate = true
-					}
-
-					// Effectuer une opération XOR pour dessiner le pixel (gestion de collision)
-					c.screen[posY*64+posX] = c.screen[posY*64+posX] != (pixel == 1)
-				}
+			if c.stackPointer == 0 {
+				log.Panic("Attempted to return from an empty stack")
 			}
-
-		case 0xF00A:
-			x := uint16((opcode & 0x0F00) >> 8)
-
-			// Vérifiez si une touche est pressée
-			keyPressed := false
-			for i := 0; i < len(c.input); i++ {
-				if c.input[i] {
-					c.command[x] = byte(i)
-					keyPressed = true
-					break
-				}
+			c.stackPointer--
+			c.programCounter = c.stack[c.stackPointer]
+		case 0x00E0:
+			for i := range c.screen {
+				c.screen[i] = 0
 			}
-
-			if !keyPressed {
-				// Si aucune touche n'est pressée, réexécutez cet opcode
-				c.programCounter -= 2
-			}
-
-		case 0xF015:
-			x := uint16((opcode & 0x0F00) >> 8)
-			c.delayTimer = c.command[x]
-			c.programCounter += 2
-
-		case 0xF018:
-			x := uint16((opcode & 0x0F00) >> 8)
-			c.soundTimer = c.command[x]
-			c.programCounter += 2
-
-			c.programCounter += 2
-
 			c.programCounter += 2
 		default:
 			fmt.Printf("Unknown opcode: 0x%X\n", opcode)
 		}
+	case 0xF000:
+		switch opcode & 0x00FF {
+		case OpcodeFX29:
+			x := uint16((opcode & 0x0F00) >> 8)
+			character := c.command[x]
+			c.indexRegister = uint16(character) * 5
+			c.programCounter += 2
+		case OpcodeFX33:
+			x := uint16((opcode & 0x0F00) >> 8)
+			value := c.command[x]
+			c.memory[c.indexRegister] = value / 100
+			c.memory[c.indexRegister+1] = (value / 10) % 10
+			c.memory[c.indexRegister+2] = value % 10
+			c.programCounter += 2
+
+		default:
+			fmt.Printf("Unknown opcode: 0x%X\n", opcode)
+		}
+	case 0xD000:
+		println("case draw")
+		x := uint16((opcode & 0x0F00) >> 8)
+		y := uint16((opcode & 0x00F0) >> 4)
+		height := uint16(opcode & 0x000F)
+
+		for row := uint16(0); row < height; row++ {
+			spriteData := c.memory[c.indexRegister+row]
+			for col := uint16(0); col < 8; col++ {
+				pixel := (spriteData >> (7 - col)) & 0x1
+				index := x + row + (y + col*64)
+
+				if pixel == 1 {
+
+					c.screen[index] = 1
+				} else {
+					c.screen[index] = 0
+				}
+				println(c.screen[index])
+
+			}
+		}
+
 	}
 }
 
@@ -211,20 +117,21 @@ type Game struct {
 }
 
 func (g *Game) Update() error {
-	// Gérer les événements de touche (exemple avec les touches 0-9)
-	if ebiten.IsKeyPressed(ebiten.Key0) {
-		g.chip8.input[0x0] = true
-	} else {
-		g.chip8.input[0x0] = false
+	for i := 0; i <= 9; i++ {
+		if ebiten.IsKeyPressed(ebiten.Key(byte(i) + byte(ebiten.Key0))) {
+			g.chip8.input[byte(i)] = true
+		} else {
+			g.chip8.input[byte(i)] = false
+		}
 	}
-	// Répétez ce processus pour les autres touches (0-9)...
-	// Par exemple, pour la touche 1 : g.chip8.Input[0x1] = ebiten.IsKeyPressed(ebiten.Key1)
+
+	g.chip8.fetchOpcode()
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello World") //display on screen
+	ebitenutil.DebugPrint(screen, "")
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -233,18 +140,21 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("CHIP8 EMULATOR") // titre de la fenetre
+	ebiten.SetWindowTitle("CHIP8 EMULATOR")
 
-	// Créez une instance de Chip8
 	chip8 := &Chip8{}
 
-	// Chargez la ROM du jeu
-	if err := chip8.loadProgram(); err != nil {
+	romFileName := "game/1-chip8-logo.ch8"
+	romData, err := os.ReadFile(romFileName)
+	if err != nil {
+		log.Fatalf("Erreur lors de la lecture du fichier ROM : %v", err)
+	}
+
+	if err := chip8.loadProgram(romData); err != nil {
 		log.Fatalf("Erreur lors du chargement de la ROM : %v", err)
 	}
 
-	// Lancez l'émulateur
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(&Game{chip8: *chip8}); err != nil {
 		log.Fatal(err)
 	}
 }
